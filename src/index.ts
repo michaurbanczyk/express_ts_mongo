5c78a31560fc75368fc015a96602a92e12389152
Michal Urbanczyk <michal.urbanczyk@grapeup.com>
2023-07-30 13:08:28 +0200

    Project initialization

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// Required External Modules
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import * as dotenv from 'dotenv';
+
+// App Variables
+dotenv.config();
+const port = process.env.PORT;
+
+// App Configuration
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Server Activation
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
